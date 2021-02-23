aa0109e25b8702e8aef14717b2177f145ec7ed8d
LurkErLV <62105039+LurkErLV@users.noreply.github.com>
2021-02-23 09:35:26 +0200

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, modi ex! Distinctio accusantium dicta error sapiente nemo nam! Cumque, cum labore. Similique sapiente, harum quis pariatur dolorum ipsum praesentium adipisci!</p>
+    
+    <router-link to="/todos">todos</router-link>
+    </div>
+</template>
